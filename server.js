301c6811eda968e3e10f4532363aa1fe31042511
dcaueb78 <caue1017@hotmail.com>
2019-10-31 17:00:40 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,11 @@
+Client = require("rpc-client");
+
+client = new Client({host:"localhost",port:"8080", protocol:"https"});
+
+client.setBasicAuth("caue", "batatinha123");
+
+client.call("RegisterUserSoap", [1,2,4], function(err, result){
+  if(!err) {
+    console.log("Resultado: " + result);
+  }
+})
